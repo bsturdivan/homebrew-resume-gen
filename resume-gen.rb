83050240fa58e9e8dff53432e194c59230bd21c0
Brian Sturdivan <bsturdivan@gmail.com>
2024-09-21 17:52:48 -0700

    create tap

---
+++ resume-gen.rb
@@ -0,0 +1,15 @@
+class ResumeGen < Formula
+  desc "Generate a pdf resume from the Github markup language"
+  homepage "https://github.com/bsturdivan/resume_generator"
+  url "https://github.com/bsturdivan/resume_generator/releases/download/v1.0.0/resume_generator.tar.gz"
+  sha256 "e78eb204e4af3a3ab00f417820a2a974c5d16ecd83d3a3ba2efee30a5e76785e"
+  license "MIT"
+
+  def install
+    bin.install bin/console
+  end
+
+  test do
+    system "#{bin}/console"
+  end
+end
